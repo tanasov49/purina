c99733831e5b69e0603829d1c1d41e03f9f54721
tanasov49 <tanasov49@gmail.com>
2022-04-20 15:45:48 +0200

    new project

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const section = document.querySelector('address-clients');
+const requestURL = '../data/json.json';
+const request = new XMLHttpRequest();
+const templateDbc = document.querySelector('.template-dbc');
+const dbcCodes = document.querySelector('.dbc-codes');
+request.open('GET', requestURL);
+request.responseType = 'text'; // now we're getting a string!
+request.send();
+
+request.onload = function() {
+  const superHeroesText = request.response;
+  const superHeroes = JSON.parse(superHeroesText);
+  showHeroes(superHeroes);
+}
+function showHeroes(jsonObj) {
+    const heroes = jsonObj;
+
+    for (let i = 0; i < heroes.length; i++) {
+      const myArticle = document.querySelector('.address');
+      const myH2 = document.createElement('option');
+      myH2.className = 'address__option';
+      myH2.textContent = heroes[i]["Address"]; 
+    myArticle.appendChild(myH2);
+  }
+  for (let i = 0; i < heroes.length; i++) {
+    const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode(true);
+    const dbcText = document.createElement('p');
+    dbcText.className = 'dbc-list__text';
+    dbcText.textContent = heroes[i]["Address"];
+    dbcList.appendChild(dbcText);
+    dbcCodes.appendChild(dbcList);
+  }
+
+}
