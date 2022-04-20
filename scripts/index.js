e1acc419f90869734e17fadf4d79fcebe95820aa
tanasov49 <tanasov49@gmail.com>
2022-04-20 17:51:21 +0200

    dbc update

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const section = document.querySelector('address-clients');
 const requestURL = '../data/json.json';
 const request = new XMLHttpRequest();
 const templateDbc = document.querySelector('.template-dbc');
-const dbcCodes = document.querySelector('.dbc-codes');
+
 request.open('GET', requestURL);
 request.responseType = 'text'; // now we're getting a string!
 request.send();
@@ -15,23 +15,23 @@ request.onload = function() {
 function showHeroes(jsonObj) {
     const heroes = jsonObj;
 
-
     for (let i = 0; i < heroes.length; i++) {
       const myArticle = document.querySelector('.address');
+      const dbcCodes = document.querySelector('.dbc-codes');
+      const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode(true);
       const myH2 = document.createElement('option');
       myH2.className = 'address__option';
-      myH2.textContent = heroes[i]["Address"]; 
-    myArticle.appendChild(myH2);
-    const dbc = Object.keys(jsonObj[i]);
-    for (let j = 0; j < dbc.length; j++) {
-      const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode(true);
+      myH2.textContent = heroes[i]["Address"];
+      
+    const dbc = Object.keys(heroes[i]);
+    for (let j = 1; j < dbc.length; j++) {
       const dbcText = document.createElement('p');
       dbcText.className = 'dbc-list__text';
       dbcText.textContent = dbc[j];
       dbcList.appendChild(dbcText);
       dbcCodes.appendChild(dbcList);
     }
+    myArticle.appendChild(myH2);
+  }
   }
 
-
-}
