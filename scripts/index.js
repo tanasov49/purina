dd6d7be28e84eefba772000120e2fffb6be48821
tanasov49 <tanasov49@gmail.com>
2022-04-21 17:41:58 +0200

    111

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const select = document.querySelector('.address');
-
+const templateDbc = document.querySelector('.template-dbc');
+const dbcCodes = document.querySelector('.dbc-codes');
 const showAddress = (card) => {
   const addressTitle = document.createElement('option');
   addressTitle.textContent = card["Address"];
@@ -11,9 +12,71 @@ const addNewElements = dbc.map((card) => {
 });
 select.append(...addNewElements);
 
-const showDbcCodes = (card) => {
-  
-}
+dbc.forEach(function (dbc) {
+  const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode();
+  const dbcText = document.createElement('p');
+  const dbcValue = Object.keys(dbc);
+  dbcText.className = 'dbc-list__text';
+  for (let i = 0; i >= 0; i++) {
+
+  }
+  dbcText.textContent = dbcValue[i];
+  dbcList.appendChild(dbcText);
+  dbcCodes.appendChild(dbcList);
+});
+
+
+// const showDbcCodes = (code) => {
+//   const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode();
+//   const dbcText = document.createElement('p');
+//   const dbcValue = Object.keys(code);
+//   for (let i = 0; i < dbcValue.length; i++) {
+//     dbcText.className = 'dbc-list__text';
+//     dbcText.textContent = dbcValue[i];
+//     dbcList.appendChild(dbcText);
+//   }
+//   dbcList.appendChild(dbcCodes);
+// }
+
+
+// const section = document.querySelector('address-clients');
+// const requestURL = '../data/json.json';
+// const request = new XMLHttpRequest();
+// const templateDbc = document.querySelector('.template-dbc');
+
+// request.open('GET', requestURL);
+// request.responseType = 'text'; // now we're getting a string!
+// request.send();
+
+// request.onload = function() {
+//   const superHeroesText = request.response;
+//   const superHeroes = JSON.parse(superHeroesText);
+//   showHeroes(superHeroes);
+// }
+// function showHeroes(jsonObj) {
+//     const heroes = jsonObj;
+
+//     for (let i = 0; i < heroes.length; i++) {
+//       const myArticle = document.querySelector('.address');
+//       const dbcCodes = document.querySelector('.dbc-codes');
+//       const dbcList = templateDbc.content.querySelector('.dbc-list').cloneNode(true);
+//       const myH2 = document.createElement('option');
+//       myH2.className = 'address__option';
+//       myH2.textContent = heroes[i]["Address"];
+      
+//     const dbc = Object.keys(heroes[i]);
+//     for (let j = 1; j < dbc.length; j++) {
+//       const dbcText = document.createElement('p');
+//       dbcText.className = 'dbc-list__text';
+//       dbcText.textContent = dbc[j];
+//       dbcList.appendChild(dbcText);
+//       dbcCodes.appendChild(dbcList);
+//     }
+//     myArticle.appendChild(myH2);
+//   }
+//   }
+
+
 
 // const createCard = (card) => {
 
